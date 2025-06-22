c4aa1eaac04b354e78c408190bc7d24d562a50a2
vivitha1703 <vivitha17m@gmail.com>
2025-06-22 19:01:08 +0530

    deployment

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
+const app = require('./app');         // ← loads app.js
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const app = require('./app');
-const mongoose = require('mongoose');
-
-const PORT = process.env.PORT || 5000;
-
-console.log("Mongo:", process.env.MONGO_URI); // for debug
+const PORT = process.env.PORT;
+if (!PORT) throw new Error("PORT must be defined");
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
